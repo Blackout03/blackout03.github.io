f48918475aa39ebfb554d291b3269bd1c96f875e
Blackout03 <gardner.hayden03@gmail.com>
2025-03-05 01:21:10 +1100

    Added Settings menu, easy way to change version instead of just the url param, url param still works cause I use it

---
+++ minecraft/settings.js
@@ -0,0 +1,5 @@
+﻿// Toggle the settings menu when the button is clicked
+document.getElementById('settings-btn').addEventListener('click', function() {
+	const settingsMenu = document.getElementById('settings-menu');
+	settingsMenu.classList.toggle('open');
+});
